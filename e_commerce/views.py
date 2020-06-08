63d0039b6061cd99a73d539d10e6ad5dab2db3f7
Heloisa <heloisaguimaraes86@gmail.com>
2020-06-08 18:22:46 -0300

    Fixing any bugs in views.py and adding some codes in template/about/view.html and template/about/view.html

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -4,21 +4,21 @@ from django.shortcuts import render
 
 def home_page(request):
     context = {
-        "title" : "Main page"
+        "title" : "Main page",
         "content": "Welcome to main page!"
     }
     return render(request, "home_page.html", context)
 
 def about_page (request):
     context = {
-        "title": "About page"
+        "title": "About page",
         "content": "Welcome to about page!"
     }
-    return render (request, "about/view.html", context)
+    return render(request, "about/view.html", context)
 
 def contact_page (request):
     context = {
-        "title": "Contact page"
+        "title": "Contact page",
         "content": "Welcome to contact page!"
     }
     return render (request, "contact/view.html", context)
