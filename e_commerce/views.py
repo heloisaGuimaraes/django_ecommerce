6c950a9ed635f4c12e469b6259f8aee0dc51ca30
Heloisa <heloisaguimaraes86@gmail.com>
2020-06-08 18:04:33 -0300

    Creating template/about/view.py and template/contact/view.py. Doing functions related contact and about pages and updating home_page function

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,4 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
+
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title" : "Main page"
+        "content": "Welcome to main page!"
+    }
+    return render(request, "home_page.html", context)
+
+def about_page (request):
+    context = {
+        "title": "About page"
+        "content": "Welcome to about page!"
+    }
+    return render (request, "about/view.html", context)
+
+def contact_page (request):
+    context = {
+        "title": "Contact page"
+        "content": "Welcome to contact page!"
+    }
+    return render (request, "contact/view.html", context)
