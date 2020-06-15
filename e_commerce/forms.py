28ed656c374a5c06de31ee97782eb10cde50f5ca
Heloisa <heloisaguimaraes86@gmail.com>
2020-06-15 18:28:37 -0300

    Creating contact page

--- e_commerce/forms.py
+++ e_commerce/forms.py
@@ -1,6 +1,24 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    nome_completo = forms.CharField()
-    email = forms.Email.Field()
-    conteudo = forms.CharField()
+    nome_completo = forms.CharField(error_messages={'required': 'Campo obrigatório.'},
+        widget = forms.TextInput(
+        attrs = {
+            "class": "form-control", 
+            "placeholder": "Digite seu nome completo."}))
+    
+    email = forms.EmailField(error_messages={'required': 'Campo obrigatório'},
+        widget = forms.EmailInput(
+        attrs = {"class": "form-control", 
+                 "placeholder": "Digite seu e-mail."}))
+    
+    conteudo = forms.CharField(error_messages={'required': 'Campo obrigatório'},
+        widget = forms.Textarea(
+        attrs = {"class": "form-control", 
+                 "placeholder": "Digite sua mensagem."}))
+    def clean_email(self):
+        
+        email = self.cleaned_data.get("email")
+        if not "gmail.com" in email:
+            raise forms.ValidationError("O E-mail deve ser do gmail.com")
+        return email
